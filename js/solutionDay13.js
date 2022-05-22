90022410e2152e4c39f8c644b7fb8c76888d342a
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-05-22 18:32:56 -0400

    Day 13 function that multiplies two inputs

---
+++ js/solutionDay13.js
@@ -0,0 +1,15 @@
+// Day 13 function that multiples two numbers. 
+//P: multiply, x = first number, y = the second number to multiply
+//R: number
+//E: Given x = 2 and y = 3 your solution will return 6
+//   Given  x = 4 and y = 6 your solution will return 24
+
+function multiply(x, y) {
+    //math for both and set as sum, then return sum to console. 
+    sum = x * y
+    return sum
+  }
+  
+  // Or using arrow functions
+  
+  // let multiply = (a, b) => a * b;
