48e41a6cac5fe9ef2423e5992d24ec4e007a38df
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-05-27 21:55:03 -0400

    Day 18 Counting truthy sheep

---
+++ js/solutionDay18.js
@@ -0,0 +1,15 @@
+// Day 18 Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// P: Array - true for present and false for not
+// E: Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    //const arrayOfSheep = [true, false, true, false, true];
+    const count = arrayOfSheep.filter(Boolean).length;
+    return count
+  }
+
+
+// Or using arrow function
+
+//const countSheeps = arrayOfSheeps => arrayOfSheeps.filter(s => s).length;
