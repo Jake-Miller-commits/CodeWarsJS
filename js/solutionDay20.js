aadeeb46b78f9b0a55b619cdbe6d6f49c1a65921
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-05-29 21:13:56 -0400

    Day 20 string to number function

---
+++ js/solutionDay20.js
@@ -0,0 +1,10 @@
+// Day 20 We need a function that can transform a string into a number. What ways of achieving this do you know?
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+// P: str - "1"
+// E: For example, for "1", return 1
+
+
+const stringToNumber = function(str){
+    str = parseInt(str, 10);
+    return str
+  }
