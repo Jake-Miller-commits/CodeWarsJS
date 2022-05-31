a2cca1956c40c304668b65377f5d87f6f1e0e161
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-05-31 18:55:32 -0400

    Day 21 template literal input into string

---
+++ js/solutionDay21.js
@@ -0,0 +1,11 @@
+// Day 21 Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// Note: [Make sure you type the exact thing I wrote or the program may not execute properly]
+// P: name
+// R: return
+// E: For example, for "Jake" - "Hello, Jake how are you doing today?"
+
+function greet(name){
+    //using a template literal to input the name into the string
+    return `Hello, ${name} how are you doing today?`
+  }
