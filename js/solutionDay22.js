97df46eccfe31ff681aad02ce8692aca5ae52817
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-06-02 18:30:25 -0400

    Day 22 boolean to string

---
+++ js/solutionDay22.js
@@ -0,0 +1,28 @@
+// Day 22 Implement a function which convert the given boolean value into its string representation. Note: Only valid inputs will be given.
+
+// P: b - true or false
+// R: return
+// E: For example, for true - "true" or false - "false"
+
+// function booleanToString(b){
+//     if (b === true) {
+//         results = "true"
+//         return results
+//     } else {
+//         results = "false"
+//         return results
+//     }
+//   }
+
+
+// Or using Arrow function
+
+function booleanToString(b){
+    return b.toString();
+  }
+
+// OR 
+
+// function booleanToString(b){
+//     return b ? 'true' : 'false';
+//   }
