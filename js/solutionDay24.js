902e211cca129ded77a608e79d57fd389e1fcffe
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-06-07 18:56:28 -0400

    Day 24 First Initials function

---
+++ js/solutionDay24.js
@@ -0,0 +1,26 @@
+// Day 24 Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// P:
+// R:
+// Example
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+
+function abbrevName(name){
+    //using Array split and index position
+    var nameArray = name.split(" ");
+    return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+  }
+
+// OR arrow function
+
+
+// function abbrevName(name){
+
+//     return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+
+// }
