6f6df13d23f4a592cc09491810ea1ffba784c691
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-06-15 18:44:09 -0400

    Day 27 Is divisible by two numbers

---
+++ js/solutionDay27.js
@@ -0,0 +1,26 @@
+// Day 27 Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+// P: n - is divisable by, x and y
+// R: Boolean - true or false
+
+// Examples:
+// 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+// 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+// 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+// 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+
+function isDivisible(n, x, y) {
+    if (!(n % x) && !(n % y)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// OR
+// const isDivisible = (n, x, y) => n%x==0 && n%y==0;
+
+// OR IF
+// function isDivisible(n, x, y) {
+//     return n % x === 0 && n % y === 0
+// }
