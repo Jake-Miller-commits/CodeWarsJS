2d66508014f7aecff9b263426e662b31881f3282
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-06-16 22:16:21 -0400

    Changed to cleaner function

--- js/solutionDay27.js
+++ js/solutionDay27.js
@@ -9,18 +9,18 @@
 // 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
 // 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
 
-function isDivisible(n, x, y) {
-    if (!(n % x) && !(n % y)) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// function isDivisible(n, x, y) {
+//     if (!(n % x) && !(n % y)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
 // OR
 // const isDivisible = (n, x, y) => n%x==0 && n%y==0;
 
 // OR IF
-// function isDivisible(n, x, y) {
-//     return n % x === 0 && n % y === 0
-// }
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0
+}
