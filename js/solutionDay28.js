270cafd57d1de351b5eed1f368856b0ad9daf3f5
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-06-18 22:23:55 -0400

    Moved to a different structure

--- js/solutionDay28.js
+++ js/solutionDay28.js
@@ -11,10 +11,10 @@
 // time = 6.7---> litres = 3
 // time = 11.8--> litres = 5
 
-function litres(time) {
-    return Math.floor(time * 0.5);
-  }
+// function litres(time) {
+//     return Math.floor(time * 0.5);
+//   }
 
 // OR arrow function
 
-// const litres = time => Math.floor(time * 0.5);
+const litres = time => Math.floor(time * 0.5);
