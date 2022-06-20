654ddafed1d4c7e0d8b49d6ff6fd50110d0b779a
Jacob Miller <63867473+Jake-Miller-commits@users.noreply.github.com>
2022-06-19 22:34:04 -0400

    Day 29 Double the number

---
+++ js/solutionDay29.js
@@ -0,0 +1,16 @@
+// Day 29 Create a function , to double an integer.
+
+// P: i - integer to double
+// R: return - number
+
+// Examples:
+// i = 3 ---->  6
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return (i * 2);
+  }
+
+// OR arrow function
+
+//const doubleInteger = i => 2 * i;
